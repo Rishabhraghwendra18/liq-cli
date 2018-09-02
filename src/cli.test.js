499b814cc685e9f45cfbd57dd9237a6662b94968
Zane Rockenbaugh <zane@liquid-labs.com>
2018-09-01 22:01:48 -0500

    [cli.test.js] test no input
    also suppress re-echoing expected error output

--- src/cli.test.js
+++ src/cli.test.js
@@ -5,13 +5,39 @@ const execOpts = {
   silent: true,
 }
 
-test('invalid global action results in error', () => {
+const expectedUsage = expect.stringMatching(`Usage:
+
+proj <component | global action> <action> [<action args>]
+
+global actions:\\.+`)
+
+test('no argument results in usage and error', () => {
+  console.error = jest.fn() // supresses err echo from shelljs
+  const result = shell.exec(`gcproj`, execOpts)
+  const expectedErr = expect.stringMatching(
+    new RegExp(`Invalid invocation. See usage above.\\s*`))
+
+  expect(result.stdout).toEqual(expectedUsage)
+  expect(result.stderr).toEqual(expectedErr)
+  expect(result.code).toBe(1)
+})
+
+test('invalid global action results in usage and error', () => {
   const badGlobal = 'no-such-global-action'
+  console.error = jest.fn() // supresses err echo from shelljs
   const result = shell.exec(`gcproj ${badGlobal}`, execOpts)
   const expectedErr = expect.stringMatching(
     new RegExp(`No such component or global action '${badGlobal}'.\\s*`))
 
-  expect(result.stdout).toBe('')
+  expect(result.stdout).toEqual(expectedUsage)
   expect(result.stderr).toEqual(expectedErr)
   expect(result.code).toBe(1)
+})
+
+test('help should print usage', () => {
+  const result = shell.exec(`gcproj help`, execOpts)
+
+  expect(result.stdout).toEqual(expectedUsage)
+  expect(result.stderr).toEqual('')
+  expect(result.code).toBe(0)
 })
