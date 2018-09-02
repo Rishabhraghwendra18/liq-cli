23158a6d42cad371fbf06274a41160e5feb0ece8
Zane Rockenbaugh <zane@liquid-labs.com>
2018-09-02 17:27:29 -0500

    [inc.sh] removed debug

--- src/actions/inc.sh
+++ src/actions/inc.sh
@@ -1,6 +1,5 @@
 getActions() {
   local MY_DIR=`dirname "${BASH_SOURCE[0]}"`
-  echo "MY_DIR: ${MY_DIR}"
   for d in `find "${MY_DIR}" -type d -maxdepth 1 -not -path "${MY_DIR}"`; do
     for f in "${d}/"*.sh; do echo "f: ${f}"; source "$f"; done
   done
