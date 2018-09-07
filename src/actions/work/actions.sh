1423dfb97341303c2fc1aef253b9ef77b7b05f26
Zane Rockenbaugh <zane@liquid-labs.com>
2018-09-07 15:34:32 -0500

    fixed wording on line change count

--- src/actions/work/actions.sh
+++ src/actions/work/actions.sh
@@ -43,7 +43,7 @@ work-merge() {
   # git branch -qd "$WORKBRANCH" \
   # || echoerr "Could not delete '${WORKBRANCH}'. This can happen if the branch was renamed."
   # TODO: provide a reference for checking the merge is present and if safe to delete.
-  echo "Line count change: $DIFF_COUNT"
+  echo "Line change count: $DIFF_COUNT"
 }
 
 work-diff-master() {
