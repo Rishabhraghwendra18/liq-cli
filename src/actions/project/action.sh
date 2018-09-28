5c8156d093d6c9bede694989c33b435149cbc226
Zane Rockenbaugh <zane@liquid-labs.com>
2018-09-28 14:58:05 -0500

    fixed issues and rationalized testing
    reworked the setup and teardown to happen as part of testing since it was relying on catalyst anyway and for the project and work tests (which need to be merged in any case) to be sequential

--- src/actions/project/action.sh
+++ src/actions/project/action.sh
@@ -1,32 +1,33 @@
 project-init_git_setup() {
-  local ORIGIN_URL=`git config --get remote.origin.url || true`
-  if [[ -z "$ORIGIN_URL" ]]; then
-    local HAS_FILES=`ls -a "${BASE_DIR}" | wc -w`
-    local IS_GIT_REPO
-    if [[ -d "${BASE_DIR}"/.git ]]; then
-      IS_GIT_REPO='true'
-    else
-      IS_GIT_REPO='false'
-    fi
+  local HAS_FILES=`ls -a "${BASE_DIR}" | (grep -ve '^\.$' || true) | (grep -ve '^\.\.$' || true) | wc -w`
+  local IS_GIT_REPO
+  if [[ -d "${BASE_DIR}"/.git ]]; then
+    IS_GIT_REPO='true'
+  else
+    IS_GIT_REPO='false'
+  fi
+  # first we test if set externally as environment variable (used in testing).
+  if [[ -z "${ORIGIN_URL}" ]]; then
+    ORIGIN_URL=`git config --get remote.origin.url || true`
     if [[ -z "${ORIGIN_URL:-}" ]]; then
-      if (( $HAS_FILES == 0 )) && [[ $IS_GIT_REPO == 'false' ]]; then
-        echo "The origin will be cloned, if provided."
-      elif [[ -n "$ORIGIN_URL" ]] && [[ $IS_GIT_REPO == 'false' ]]; then
-        echo "The current directory will be initialized as a git repo with the provided origin."
-      else
-        echo "The origin of this existing git repo will be set, if provided."
+      if [[ -z "${ORIGIN_URL:-}" ]]; then
+        if (( $HAS_FILES == 0 )) && [[ $IS_GIT_REPO == 'false' ]]; then
+          echo "The origin will be cloned, if provided."
+        elif [[ -n "$ORIGIN_URL" ]] && [[ $IS_GIT_REPO == 'false' ]]; then
+          echo "The current directory will be initialized as a git repo with the provided origin."
+        else
+          echo "The origin of this existing git repo will be set, if provided."
+        fi
+        read -p 'git origin URL: ' ORIGIN_URL
       fi
-      read -p 'git origin URL: ' ORIGIN_URL
-    fi
+    fi # -z "$ORIGIN_URL" - git test
+  fi # -z "$ORIGIN_URL" - external / global
 
-    if [[ -n "$ORIGIN_URL" ]] && (( $HAS_FILES == 0 )) && [[ $IS_GIT_REPO == 'false' ]]; then
-      git clone -q "$ORIGIN_URL" "${BASE_DIR}" && echo "Cloned '$ORIGIN_URL' into '${BASE_DIR}'."
-    elif [[ -n "$ORIGIN_URL" ]] && [[ $IS_GIT_REPO == 'false' ]]; then
-      git init "${BASE_DIR}"
-    fi
-    if [[ -n "$ORIGIN_URL" ]]; then
-      git remote add origin "$ORIGIN_URL"
-    fi
+  if [[ -n "$ORIGIN_URL" ]] && (( $HAS_FILES == 0 )) && [[ $IS_GIT_REPO == 'false' ]]; then
+    git clone -q "$ORIGIN_URL" "${BASE_DIR}" && echo "Cloned '$ORIGIN_URL' into '${BASE_DIR}'."
+  elif [[ -n "$ORIGIN_URL" ]] && [[ $IS_GIT_REPO == 'false' ]]; then
+    git init "${BASE_DIR}"
+    git remote -q add origin "$ORIGIN_URL"
   fi
 
   if [[ -d "${BASE_DIR}/.git" ]]; then
