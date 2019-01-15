46f89c37fd8de41200fe1f0fc4e3afc8df83af67
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-14 20:42:51 -0600

    sketched out new service usage
    also, the environment commands should migrate under runtime

--- src/actions/runtime/usage.sh
+++ src/actions/runtime/usage.sh
@@ -1,10 +1,31 @@
-print_service_usage() {
-  local PREFIX="${1:-}"
-  if [[ -z "$PREFIX" ]]; then
-    echo -e "Valid service actions are:\n"
+usage-runtime-environment() {
+  local TAB="${1:-}"
+  local PREFIX="${2:-}"
+
+  echo "${TAB}${PREFIX}environment [<name>|-a [<name>]|-d [<name>...]] :"
+  echo "${TAB}  no args : Lists environments for the current project."
+  echo "${TAB}  <name>... : Gives detailed info for each named environment."
+  echo "${TAB}  -a [<name>] : Interactively adds a new environment definition."
+  echo "${TAB}  -d <name>... : Deletes the named environments associated to the current project."
+}
+
+usage-runtime-services() {
+  local TAB="${1:-}"
+  local PREFIX="${2:-}"
+
+  echo "${TAB}${PREFIX}service [<iface class>|-a [<iface class>]|-d [<iface class>...]] :"
+  echo "${TAB}  no args : Lists runtime services for the current environment and their status."
+  echo "${TAB}  <iface class>... : Gives detailed info for service of the named interface class."
+  echo "${TAB}  -s [<iface class>] : Starts the service by interface class or all services (no args)."
+  echo "${TAB}  -S [<iface class>] : Stops the service by interface class or all services (no args)."
+  echo "${TAB}  -r [<iface class>] : Restarts the service by interface class or all services (no args)."
+}
+
+print_runtime_usage() {
+  local TAB="${1:-}"
+  if [[ -z "$TAB" ]]; then
+    echo -e "Runtime commands:\n"
   fi
-  echo "${PREFIX}list : List currently available services."
-  echo "${PREFIX}start [all|<name>] : Started the named, all, or selected service."
-  echo "${PREFIX}stop [all|<name>] : Started the named, all, or selected service."
-  echo "${PREFIX}restart [all|<name>] : Started the named, all, or selected service."
+  usage-runtime-environment "$TAB"
+  usage-runtime-services "$TAB"
 }
