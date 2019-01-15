1ce33f8b1291f53863389762af0c3da3d9a5bbfb
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-15 16:30:02 -0600

    implemented provides-service -d

--- src/actions/project/actionslib/provides-service.sh
+++ src/actions/project/actionslib/provides-service.sh
@@ -6,6 +6,9 @@ project-provides-service() {
   elif [[ "$1" == '-a' ]]; then # add
     shift
     project-provides-service-add "$@"
+  elif [[ "$1" == '-d' ]]; then # delete
+    shift
+    project-provides-service-delete "$@"
   else # show detail on each named service
     while [[ $# -gt 0 ]]; do
       echo "$1:"
@@ -85,5 +88,20 @@ EOF
   done
 
   PACKAGE=`echo "$PACKAGE" | jq ". + { \"$CAT_PROVIDES_SERVICE\": (.\"$CAT_PROVIDES_SERVICE\" + [$SERVICE_DEF]) }"`
+  echo "$PACKAGE" | jq > "$PACKAGE_FILE"
+}
+
+project-provides-service-delete() {
+  if (( $# == 0 )); then
+    echoerrandexit "Must specify service names to delete."
+  fi
+
+  local SERV_NAME
+  for SERV_NAME in "$@"; do
+    if echo "$PACKAGE" | jq -e "(.\"$CAT_PROVIDES_SERVICE\" | map(select(.name == \"$SERV_NAME\")) | length) == 0" > /dev/null; then
+      echoerr "Did not find service '$SERV_NAME' to delete."
+    fi
+    PACKAGE=`echo "$PACKAGE" | jq "setpath([\"$CAT_PROVIDES_SERVICE\"]; .\"$CAT_PROVIDES_SERVICE\" | map(select(.name != \"$SERV_NAME\")))"`
+  done
   echo "$PACKAGE" | jq > "$PACKAGE_FILE"
 }
