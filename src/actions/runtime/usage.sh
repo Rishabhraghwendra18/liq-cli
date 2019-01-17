fa436509c078f389460d811e919937742a7674da
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-17 15:52:06 -0600

    noted updated service iface matching routine
    I didn't plan this out, but it's worked out nicely.

--- src/actions/runtime/usage.sh
+++ src/actions/runtime/usage.sh
@@ -1,6 +1,9 @@
 usageHelperServiceSpec() {
+  local TAB="${1:-}"
   echo
-  echo "Where '${cyan}<service spec>${reset}' is either a service interface class or <service iface>.<service name>."
+  echo "${TAB}Where '${cyan}<service spec>${reset}' is either a service interface class or <service iface>.<service name>. A service may"
+  echo "${TAB}be selected by it's major type, so 'sql' woud select environment services 'sql' and 'sql-mysql' (etc.)."
+  echo "${TAB}Thus, 'catalyst runtime services connect sql' may be used to connect to both MySQL, Postgres, etc. DBs."
 }
 
 usage-runtime-environments() {
@@ -28,7 +31,8 @@ usage-runtime-services() {
   echo "${TAB}  err-log [<service spec>...] : Displays error logs for all or named services for the current environment."
   echo "${TAB}  connect <service spec> : Connects to the named service, if possible."
 
-  if [[ -z "$TAB" ]]; then usageHelperServiceSpec; fi
+  if [[ -z "$TAB" ]]; then
+    usageHelperServiceSpec; fi
 }
 
 print_runtime_usage() {
@@ -38,5 +42,5 @@ print_runtime_usage() {
   usage-runtime-environments "${TAB}  "
   echo
   usage-runtime-services "${TAB}  "
-  usageHelperServiceSpec
+  usageHelperServiceSpec "${TAB}  "
 }
