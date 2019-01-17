2a8d51119b4c63927c007af389f1626d5741cdaa
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-17 12:03:18 -0600

    finalize runtime service usage help

--- src/actions/runtime/usage.sh
+++ src/actions/runtime/usage.sh
@@ -1,9 +1,14 @@
+usageHelperServiceSpec() {
+  echo
+  echo "Where '${cyan}<service spec>${reset}' is either a service interface class or <service iface>.<service name>."
+}
+
 usage-runtime-environments() {
   local TAB="${1:-}"
   local PREFIX
-  if [[ -z "$TAB" ]]; then PREFIX='catalyst runtime '; fi
+  if [[ -z "$TAB" ]]; then PREFIX="catalyst runtime "; fi
 
-  echo "${TAB}${PREFIX}environments [<name>|-a [<name>]|-d [<name>...]] :"
+  echo "${TAB}${PREFIX}${cyan_u}environments${reset} [<name>|-a [<name>]|-d [<name>...]] :"
   echo "${TAB}  no args : Lists environments for the current project."
   echo "${TAB}  <name>... : Gives detailed info for each named environment."
   echo "${TAB}  -a [<name>] : Interactively adds a new environment definition."
@@ -13,19 +18,25 @@ usage-runtime-environments() {
 usage-runtime-services() {
   local TAB="${1:-}"
   local PREFIX
-  if [[ -z "$TAB" ]]; then PREFIX='catalyst runtime '; fi
+  if [[ -z "$TAB" ]]; then PREFIX="catalyst runtime "; fi
 
-  echo "${TAB}${PREFIX}services [<iface class>|-a [<iface class>]|-d [<iface class>...]] :"
-  echo "${TAB}  no args : Lists runtime services for the current environment and their status."
-  echo "${TAB}  <iface class>... : Gives detailed info for service of the named interface class."
-  echo "${TAB}  -s [<iface class>] : Starts the service by interface class or all services (no args)."
-  echo "${TAB}  -S [<iface class>] : Stops the service by interface class or all services (no args)."
-  echo "${TAB}  -r [<iface class>] : Restarts the service by interface class or all services (no args)."
+  echo "${TAB}${PREFIX}${cyan_u}services${reset} :"
+  echo "${TAB}  list [<service spec>...] : Lists all or named runtime services for the current environment and their status."
+  echo "${TAB}  start [<service spec>...] : Starts all or named services for the current environment."
+  echo "${TAB}  stop [<service spec>...] : Stops all or named services for the current environment."
+  echo "${TAB}  log [<service spec>...] : Displays logs for all or named services for the current environment."
+  echo "${TAB}  err-log [<service spec>...] : Displays error logs for all or named services for the current environment."
+  echo "${TAB}  connect <service spec> : Connects to the named service, if possible."
+
+  if [[ -z "$TAB" ]]; then usageHelperServiceSpec; fi
 }
 
 print_runtime_usage() {
   local TAB="${1:-}"
-  echo -e "${TAB}catalyst runtime <sub-module>:"
+  echo "${TAB}catalyst ${cyan_u}runtime${reset} <sub-group> :"
+  echo
   usage-runtime-environments "${TAB}  "
+  echo
   usage-runtime-services "${TAB}  "
+  usageHelperServiceSpec
 }
