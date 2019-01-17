804f6f90da1ef66e248ca68c2241aa5d10d3a758
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-17 12:02:52 -0600

    flesh out bash format support

--- src/lib/_utils.sh
+++ src/lib/_utils.sh
@@ -1,6 +1,30 @@
 # https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
 red=`tput setaf 1`
 green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+purple=`tput setaf 5`
+cyan=`tput setaf 6`
+white=`tput setaf 7`
+
+bold=`tput setaf bold`
+red_b="${red}${bold}"
+green_b="${green}${bold}"
+yellow_b="${yellow}${bold}"
+blue_b="${blue}${bold}"
+purple_b="${purple}${bold}"
+cyan_b="${cyan}${bold}"
+white_b="${white}${bold}"
+
+underline=`tput smul`
+red_u="${red}${underline}"
+green_u="${green}${underline}"
+yellow_u="${yellow}${underline}"
+blue_u="${blue}${underline}"
+purple_u="${purple}${underline}"
+cyan_u="${cyan}${underline}"
+white_u="${white}${underline}"
+
 reset=`tput sgr0`
 
 echoerr() {
