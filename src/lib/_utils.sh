a480bde4a3087550c2d0b8fb079e8567467f5190
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-18 23:36:45 -0600

    setSimpleOptions now supports required and optional argument processing
    because I'm a doofus

--- src/lib/_utils.sh
+++ src/lib/_utils.sh
@@ -428,17 +428,23 @@ EOF
       VAR_NAME="$VAR_SPEC"
       SHORT_OPT=$(echo "${VAR_NAME::1}" | tr '[:upper:]' '[:lower:]')
     fi
+    local OPT_REQ=$(echo "$VAR_NAME" | sed -Ee 's/[^=]//g' | tr '=' ':')
+    VAR_NAME=`echo "$VAR_NAME" | tr -d "="`
     LOWER_NAME=`echo "$VAR_NAME" | tr '[:upper:]' '[:lower:]'`
-    LONG_OPT=`echo "${LOWER_NAME}" | tr '_' '-'`
+    LONG_OPT="$(echo "${LOWER_NAME}" | tr '_' '-')"
 
-    SHORT_OPTS="${SHORT_OPTS}${SHORT_OPT}"
-    LONG_OPTS=$( ( test ${#LONG_OPTS} -gt 0 && echo "${LONG_OPTS},") || true && echo "$LONG_OPT")
+    SHORT_OPTS="${SHORT_OPTS}${SHORT_OPT}${OPT_REQ}"
+    LONG_OPTS=$( ( test ${#LONG_OPTS} -gt 0 && echo -n "${LONG_OPTS}${OPT_REQ},") || true && echo -n "${LONG_OPT}${OPT_REQ}")
     # set on declaration so nested calles get reset
     LOCAL_DECLS="${LOCAL_DECLS}local $VAR_NAME='';"
+    local VAR_SETTER="echo \"${VAR_NAME}=true;\""
+    if [[ -n "$OPT_REQ" ]]; then
+      VAR_SETTER="echo \"${VAR_NAME}='\$2';\"; shift;"
+    fi
     CASE_HANDLER=$(cat <<EOF
     ${CASE_HANDLER}
       -${SHORT_OPT}|--${LONG_OPT}]
-        echo "${VAR_NAME}=true;"
+        $VAR_SETTER
         OPTS_COUNT=\$(( \$OPTS_COUNT + 1));;
 EOF
 )
@@ -454,7 +460,7 @@ EOF
   echo "$LOCAL_DECLS"
 
   local TMP # because of the '||', we have to break up the set
-  TMP=`${GNU_GETOPT} -o "${SHORT_OPTS}" --long "${LONG_OPTS}" -- "$@"` \
+  TMP=`${GNU_GETOPT} -o "${SHORT_OPTS}" -l "${LONG_OPTS}" -- "$@"` \
     || exit 1
   eval set -- "$TMP"
   while true; do
