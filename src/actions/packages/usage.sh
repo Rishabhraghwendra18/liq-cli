03e4e4839180fe4ebdf9ddf6855f3404b9eafa24
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-21 11:44:16 -0600

    added version-check in the usage

--- src/actions/packages/usage.sh
+++ src/actions/packages/usage.sh
@@ -6,6 +6,13 @@ ${PREFIX}${cyan_u}packages${reset} <action>:
   ${underline}build${reset} [<name>]: Builds all or the named (NPM) package in the current project.
   ${underline}audit${reset} [<name>]: Runs a security audit for all or the named (NPM) package in
     the current project.
+    IGNORE UNIGNORE:I SHOW_CONFIG:c UPDATE OPTIONS
+  ${underline}version-check${reset} [-u|update] [<name>]: Runs version check with optional
+    interactive update for all or named dependency packages.
+      [-i|--ignore|-I|--unignore] [<name>]: Configures dependency packages
+        ignored during update checks.
+      [-o|--options <option string>]: Sets options to use with 'npm-check'.
+      [-c|--show-config]: Shows the current configuration used with 'npm-check'.
   ${underline}lint${reset} [-f|--fix] [<name>]: Lints all or the named (NPM) package in the current
     project.
   ${underline}link${reset} <package>: Links (via npm) the named package to the current package.
