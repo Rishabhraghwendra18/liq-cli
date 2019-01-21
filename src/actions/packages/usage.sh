c1d6f859e76f514adce5b81b5939a2987c3c5a9b
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-21 10:56:28 -0600

    moved init to project

--- src/actions/packages/usage.sh
+++ src/actions/packages/usage.sh
@@ -3,8 +3,6 @@ usage-packages() {
 
   handleSummary "${PREFIX}${cyan_u}packages${reset} <action>: Package configuration and tools." || cat <<EOF
 ${PREFIX}${cyan_u}packages${reset} <action>:
-  ${underline}init${reset}: Configures all packages in the current repository folders as Catalyst
-    projects.
   ${underline}build${reset} [<name>]: Builds all or the named (NPM) package in the current project.
   ${underline}audit${reset} [<name>]: Runs a security audit for all or the named (NPM) package in
     the current project.
