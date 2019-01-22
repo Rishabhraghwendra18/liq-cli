9c68702f6406254bd0405bafd274f311c8125e7d
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-22 15:04:27 -0600

    implement DB memorialization

--- src/actions/data/actionslib/sql.sh
+++ src/actions/data/actionslib/sql.sh
@@ -12,6 +12,22 @@ data-build-sql() {
   local SCHEMA_FILE_COUNT=$(echo "$SCHEMA_FILES" | wc -l | tr -d ' ')
   echo "loading $SCHEMA_FILE_COUNT schema files..."
   cat $SCHEMA_FILES | services-connect sql
+  echo "Memorializing schema definations..."
+  local SCHEMA_HASH=$(cat $SCHEMA_FILES | shasum -a 256 | sed -Ee 's/ *- *$//')
+  local SCHEMA_VER_SPEC=()
+  local PACKAGE_ROOT
+  for PACKAGE_ROOT in $SCHEMA_FILES; do
+    # TODO: this breaks if 'data' appears twice in the path. We want a reluctant match, but sed doesn't support it. I guess flip to Perl?
+    PACKAGE_ROOT=$(echo "$PACKAGE_ROOT" | sed -Ee 's|/data/.+||')
+    local PACK_DEF=$(cat "${PACKAGE_ROOT}/package.json")
+    local PACK_NAME=$(echo "$PACK_DEF" | jq --raw-output '.name' | tr -d "'")
+    if ! echo "${SCHEMA_VER_SPEC[@]:-}" | grep -q "$PACK_NAME"; then
+      local PACK_VER=$(echo "$PACK_DEF" | jq --raw-output '.version' | tr -d "'")
+      SCHEMA_VER_SPEC+=("${PACK_NAME}:${PACK_VER}")
+    fi
+  done
+  SCHEMA_VER_SPEC=$(echo "${SCHEMA_VER_SPEC[@]}" | sort)
+  echo "INSERT INTO catalystdb (schema_hash, version_spec) VALUES('$SCHEMA_HASH', '${SCHEMA_VER_SPEC[@]}')" | services-connect sql
 }
 
 data-rebuild-sql() {
