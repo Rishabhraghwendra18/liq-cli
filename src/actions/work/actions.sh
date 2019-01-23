d582d9d3050b1e7468f64fafa9b031e73a3aa130
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-23 16:09:45 -0600

    added 'work involve'
    'start' and 'merge' now work with the involved projects rather than the current project

--- src/actions/work/actions.sh
+++ src/actions/work/actions.sh
@@ -13,15 +13,6 @@ work-edit() {
   cd "${BASE_DIR}" && ${OPEN_PROJ_CMD}
 }
 
-work-start() {
-  local BRANCH_DESC="${1:-}"
-  local BRANCH_NAME=`branchName "${BRANCH_DESC}"`
-  (requireArgs "${BRANCH_DESC}" \
-   && git checkout -qb "${BRANCH_NAME}" \
-   && echo "Now working on branch '${BRANCH_NAME}'.") \
-  || exit $?
-}
-
 work-ignore-rest() {
   pushd "${BASE_DIR}" > /dev/null
   touch .gitignore
@@ -32,39 +23,85 @@ work-ignore-rest() {
   popd > /dev/null
 }
 
-work-merge() {
-  git diff-index --quiet HEAD -- \
-    || echoerrandexit 'Currint working branch has uncommitted changes. Please resolve before merging.' 1
-
-  local WORKBRANCH=`git branch | (grep '*' || true) | awk '{print $2}'`
-  if [ $WORKBRANCH == 'master' ]; then
-    echoerr "Can't 'merge work' from master branch. Switch to workbranch with 'git checkout'." >&2
-    return
+work-involve() {
+  if [[ ! -L "${CATALYST_WORK_DB}/curr_work" ]]; then
+    echoerrandexit "There is no active unit of work to involve. Try:\ncatalyst work resume"
   fi
 
-  local SHORT_STAT=`git diff --shortstat master ${WORKBRANCH}`
-  local INS_COUNT=`echo "${SHORT_STAT}" | egrep -Eio -e '\d+ insertion' | awk '{print $1}' || true`
-  INS_COUNT=${INS_COUNT:-0}
-  local DEL_COUNT=`echo "${SHORT_STAT}" | egrep -Eio -e '\d+ deletion' | awk '{print $1}' || true`
-  DEL_COUNT=${DEL_COUNT:-0}
-  local DIFF_COUNT=$(( $INS_COUNT - $DEL_COUNT ))
+  local PROJECT_NAME
+  if (( $# == 0 )) && [[ -n "$BASE_DIR" ]]; then
+    PROJECT_NAME=`basename $BASE_DIR`
+  else
+    exactUserArgs PROJECT_NAME -- "$@"
+    test -d "${CATALYST_PLAYGROUND}/${PROJECT_NAME}" \
+      || echoerrandexit "Invalid project name '$PROJECT_NAME'. Perhaps it needs to be imported? Try:\ncatalyst workspace import <git URL>"
+  fi
 
-  local PUSH_FAILED=N
-  # in case the current working dir does not exist in master
-  pushd ${BASE_DIR} > /dev/null \
-  && (git checkout -q master \
-      || (popd > /dev/null && echoerrandexit "Could not switch to master branch.")) \
-  && (git merge --no-ff -qm "merge branch $WORKBRANCH" "$WORKBRANCH" \
-      || (echoerrandexit "Problem merging work branch with master. (Working directory now: '$PWD')")) \
-  && popd > /dev/null \
-  && ( (git push -q && echo "Work merged and pushed to origin.") \
-      || (PUSH_FAILED=Y && echoerr "Local merge successful, but there was a problem pushing work to master."))
-  # if we have not exited, then the merge was made and we'll attempt to clean up
-  # local work branch (even if the push fails)
-  git branch -qd "$WORKBRANCH" \
-    || echoerr "Could not delete '${WORKBRANCH}'. This can happen if the branch was renamed."
-  # TODO: provide a reference for checking the merge is present and if safe to delete.
-  echo "linecount change: $DIFF_COUNT"
+  source "${CATALYST_WORK_DB}/curr_work"
+  local BRANCH_NAME=$(basename $(readlink "${CATALYST_WORK_DB}/curr_work"))
+
+  cd "${CATALYST_PLAYGROUND}/${PROJECT_NAME}"
+  if git branch | grep -qE "^\*? *${BRANCH_NAME}\$"; then
+    echowarn "Found existing work branch '${BRANCH_NAME}' in project ${PROJECT_NAME}. We will use it. Please fix manually if this is unexpected."
+    git checkout -q "${BRANCH_NAME}" || echoerrandexit "There was a problem checking out the work branch. ($?)"
+  else
+    git checkout -qb "${BRANCH_NAME}" || echoerrandexit "There was a problem creating the work branch. ($?)"
+    echo "Created work branch '${BRANCH_NAME}' for project '${PROJECT_NAME}'."
+  fi
+
+  INVOLVED_PROJECTS="${INVOLVED_PROJECTS} ${PROJECT_NAME}"
+  updateWorkDb
+}
+
+work-merge() {
+  source "${CATALYST_WORK_DB}/curr_work"
+  local CURR_WORK=$(basename $(readlink "${CATALYST_WORK_DB}/curr_work" ))
+
+  if [[ -z "$INVOLVED_PROJECTS" ]]; then
+    echoerrandexit "No projects involved in the current unit of work '$CURR_WORK'."
+  fi
+
+  local IP
+  for IP in $INVOLVED_PROJECTS; do
+    cd "${CATALYST_PLAYGROUND}/${IP}"
+    git diff-index --quiet HEAD -- \
+      || echoerrandexit 'Current working branch has uncommitted changes. Please resolve before merging.' 1
+
+    local WORKBRANCH=`git branch | (grep '*' || true) | awk '{print $2}'`
+    if [[ "$WORKBRANCH" == "$CURR_WORK" ]]; then
+      echoerrandexit "Project '$IP' is not currently on the expected workbranch '$CURR_WORK'. Please fix and re-run."
+    fi
+  done
+
+  for IP in $INVOLVED_PROJECTS; do
+    cd "${CATALYST_PLAYGROUND}/${IP}"
+    local SHORT_STAT=`git diff --shortstat master ${WORKBRANCH}`
+    local INS_COUNT=`echo "${SHORT_STAT}" | egrep -Eio -e '\d+ insertion' | awk '{print $1}' || true`
+    INS_COUNT=${INS_COUNT:-0}
+    local DEL_COUNT=`echo "${SHORT_STAT}" | egrep -Eio -e '\d+ deletion' | awk '{print $1}' || true`
+    DEL_COUNT=${DEL_COUNT:-0}
+    local DIFF_COUNT=$(( $INS_COUNT - $DEL_COUNT ))
+
+    local PUSH_FAILED=N
+    # in case the current working dir does not exist in master
+    pushd ${BASE_DIR} > /dev/null \
+    && (git checkout -q master \
+        || (popd > /dev/null && echoerrandexit "Could not switch to master branch in project '$IP'.")) \
+    && (git merge --no-ff -qm "merge branch $WORKBRANCH" "$WORKBRANCH" \
+        || (echoerrandexit "Problem merging work branch with master for project '$IP'. ($?)")) \
+    && popd > /dev/null \
+    && ( (git push -q && echo "Work merged and pushed to origin.") \
+        || (PUSH_FAILED=Y && echoerr "Local merge successful, but there was a problem pushing work to master."))
+    # if we have not exited, then the merge was made and we'll attempt to clean up
+    # local work branch (even if the push fails)
+    git branch -qd "$WORKBRANCH" \
+      || echoerr "Could not delete '${WORKBRANCH}'. This can happen if the branch was renamed."
+    # TODO: provide a reference for checking the merge is present and if safe to delete.
+    echo "$IP linecount change: $DIFF_COUNT"
+
+    INVOLVED_PROJECTS=$(echo "$INVOLVED_PROJECTS" | sed -E "s/(^| +)$IP( +|\$)//")
+    updateWorkDb
+  done
 }
 
 work-qa() {
@@ -99,6 +136,7 @@ work-report() {
     echo "On branch: ${green}${BRANCH_NAME}"
   fi
   tput sgr0
+  # TODO: once 'git status' once and capture output
   statusReport `git status --porcelain | grep '^ M' | wc -l || true` 'modified'
   statusReport `git status --porcelain | grep '^R ' | wc -l || true` 'renamed'
   statusReport `git status --porcelain | grep '^RM' | wc -l || true` 'renamed and modifed'
@@ -112,4 +150,28 @@ work-report() {
  )
 
   tput sgr0 # TODO: put this in the exit trap, too, I think.
+}
+
+work-start() {
+  local WORK_DESC
+  exactUserArgs WORK_DESC -- "$@"
+  local WORK_DESC_SPEC='^[a-z0-9][a-z0-9-]+$'
+  # TODO: require a minimum length of 5 non-dash characters.
+  echo "$WORK_DESC" | grep -qE $WORK_DESC_SPEC \
+    || echoerrandexit "Work description must begin with a lowercase letter or number and contain only lowercase letters and numbers separated by a single dash (/$WORK_DESC_SPEC/)."
+  local BRANCH_NAME=`branchName "${WORK_DESC}"`
+
+  if [[ -f "${CATALYST_WORK_DB}/${BRANCH_NAME}" ]]; then
+    echoerrandexit "Unit of work '${BRANCH_NAME}' aready exists. Bailing out."
+  fi
+
+  touch "${CATALYST_WORK_DB}/${BRANCH_NAME}"
+  cd ${CATALYST_WORK_DB} && ln -s "${BRANCH_NAME}" curr_work
+  updateWorkDb
+
+  if [[ -n "$BASE_DIR" ]]; then
+    local CURR_PROJECT=`basename $BASE_DIR`
+    echo "Adding current project '$CURR_PROJECT' to unit of work..."
+    work-involve "$CURR_PROJECT"
+  fi
 }
