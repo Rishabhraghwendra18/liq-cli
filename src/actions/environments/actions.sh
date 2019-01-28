3d60b3f2be4d23f9913fd9f8f1c5a28a8b2d0ebc
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-28 12:24:43 -0600

    rearranged functions to alpha

--- src/actions/environments/actions.sh
+++ src/actions/environments/actions.sh
@@ -6,64 +6,6 @@ requirements-environments() {
 # TODO: move this def
 STD_ENV_PUPRPOSES='dev test pre-production production'
 
-environments-show() {
-  local ENV_NAME="${1:-}"
-
-  if [[ -n "$ENV_NAME" ]]; then
-    if [[ ! -f "${_CATALYST_ENVS}/${PACKAGE_NAME}/${ENV_NAME}" ]]; then
-      echoerrandexit "No such environment '$ENV_NAME' found for '$PACKAGE_NAME'."
-    fi
-  else
-    if [[ ! -f "${_CATALYST_ENVS}/${PACKAGE_NAME}/curr_env" ]]; then
-      echoerrandexit "No environment selected for '$PACKAGE_NAME'. Try 'catalyst environment select' or 'catalyst environment show <name>'."
-    fi
-    ENV_NAME='curr_env'
-  fi
-  cat "${_CATALYST_ENVS}/${PACKAGE_NAME}/${ENV_NAME}"
-
-  # if [[ -n "$CURR_ENV" ]] && [[ "$CURR_ENV" == "$ENV_NAME" ]]; then
-  #  echo "Current environment:"
-  #  echo "$CURR_ENV"
-  #  echo
-  #fi
-  #local ENV_DB="${_CATALYST_ENVS}/${ENV_NAME}"
-  #if [[ -f "$ENV_DB" ]]; then
-  #  cat "$ENV_DB"
-  #else
-  #  echoerrandexit "No such environment '${ENV_NAME}'."
-  #fi
-}
-
-environments-set() {
-  echoerr "TODO: sorry, 'set' implementation is outdated"
-  exit
-  local ENV_NAME KEY VALUE
-  if [[ $# -eq 3 ]]; then
-    ENV_NAME="$1"
-    KEY="$2"
-    VALUE="$3"
-  elif [[ $# -eq 2 ]]; then
-    ENV_NAME="$CURR_ENV"
-    KEY="$1"
-    VALUE="$2"
-  elif [[ $# -eq 0 ]]; then
-    ENV_NAME="$CURR_ENV"
-    echo "Select parameter to update"
-    # TODO: add 'selectOrOther' function; we use this pattern in a few places
-    select KEY in `getEnvTypeKeys` '<other>'; do break; done
-    if [[ "$KEY" == '<other>' ]]; then
-      requireAnswer 'Parameter key: ' KEY
-    fi
-    requireAnswer 'Parameter value: ' VALUE
-    updateEnvParam "$KEY" "$VALUE"
-  else
-    echoerrandexit "Unexpected number of arguments to 'catalyst environment set'."
-    # TODO: print action specific usage would be nice
-  fi
-
-  updateEnvironment
-}
-
 environments-add() {
   local ENV_NAME="${1:-}"
   # TODO: echo "Adding environment for project $CURR_PROJECT"
@@ -105,40 +47,6 @@ environments-add() {
     selectNewEnv
 }
 
-environments-list() {
-  local RESULT="$(doEnvironmentList)"
-  if test -n "$RESULT"; then
-    echo "$RESULT"
-  else
-    echo "No environments defined for '${PACKAGE_NAME}'. Use 'catalyst environment add'."
-  fi
-}
-
-environments-select() {
-  local ENV_NAME="${1:-}"
-  if [[ -z "$ENV_NAME" ]]; then
-    if test -z "$(doEnvironmentList)"; then
-      echoerrandexit "No environments defined. Try 'catalyst environment add'."
-    fi
-    echo "Select environment:"
-    select ENV_NAME in `doEnvironmentList`; do break; done
-  fi
-  local CURR_ENV_FILE="${_CATALYST_ENVS}/${PACKAGE_NAME}/curr_env"
-  if [[ -f "${_CATALYST_ENVS}/${PACKAGE_NAME}/${ENV_NAME}" ]]; then
-    test -L $CURR_ENV_FILE && rm $CURR_ENV_FILE
-    cd "${_CATALYST_ENVS}/${PACKAGE_NAME}/" && ln -s "./${ENV_NAME}" curr_env
-  else
-    echoerrandexit "No such environment '$ENV_NAME' defined."
-  fi
-  # if not error and exit
-  loadCurrEnv
-}
-
-environments-deselect() {
-  test -L $CURR_ENV_FILE && rm $CURR_ENV_FILE
-  loadCurrEnv
-}
-
 environments-delete() {
   local ENV_NAME="${1:-}"
   test -n "$ENV_NAME" || echoerrandexit "Must specify enviromnent for deletion."
@@ -171,4 +79,96 @@ environments-delete() {
   else
     echoerrandexit "No such environment '${ENV_NAME}' found. Try 'catalyst environment list'."
   fi
+}
+
+environments-deselect() {
+  test -L $CURR_ENV_FILE && rm $CURR_ENV_FILE
+  loadCurrEnv
+}
+
+environments-list() {
+  local RESULT="$(doEnvironmentList)"
+  if test -n "$RESULT"; then
+    echo "$RESULT"
+  else
+    echo "No environments defined for '${PACKAGE_NAME}'. Use 'catalyst environment add'."
+  fi
+}
+
+environments-select() {
+  local ENV_NAME="${1:-}"
+  if [[ -z "$ENV_NAME" ]]; then
+    if test -z "$(doEnvironmentList)"; then
+      echoerrandexit "No environments defined. Try 'catalyst environment add'."
+    fi
+    echo "Select environment:"
+    select ENV_NAME in `doEnvironmentList`; do break; done
+  fi
+  local CURR_ENV_FILE="${_CATALYST_ENVS}/${PACKAGE_NAME}/curr_env"
+  if [[ -f "${_CATALYST_ENVS}/${PACKAGE_NAME}/${ENV_NAME}" ]]; then
+    test -L $CURR_ENV_FILE && rm $CURR_ENV_FILE
+    cd "${_CATALYST_ENVS}/${PACKAGE_NAME}/" && ln -s "./${ENV_NAME}" curr_env
+  else
+    echoerrandexit "No such environment '$ENV_NAME' defined."
+  fi
+  # if not error and exit
+  loadCurrEnv
+}
+
+environments-set() {
+  echoerr "TODO: sorry, 'set' implementation is outdated"
+  exit
+  local ENV_NAME KEY VALUE
+  if [[ $# -eq 3 ]]; then
+    ENV_NAME="$1"
+    KEY="$2"
+    VALUE="$3"
+  elif [[ $# -eq 2 ]]; then
+    ENV_NAME="$CURR_ENV"
+    KEY="$1"
+    VALUE="$2"
+  elif [[ $# -eq 0 ]]; then
+    ENV_NAME="$CURR_ENV"
+    echo "Select parameter to update"
+    # TODO: add 'selectOrOther' function; we use this pattern in a few places
+    select KEY in `getEnvTypeKeys` '<other>'; do break; done
+    if [[ "$KEY" == '<other>' ]]; then
+      requireAnswer 'Parameter key: ' KEY
+    fi
+    requireAnswer 'Parameter value: ' VALUE
+    updateEnvParam "$KEY" "$VALUE"
+  else
+    echoerrandexit "Unexpected number of arguments to 'catalyst environment set'."
+    # TODO: print action specific usage would be nice
+  fi
+
+  updateEnvironment
+}
+
+environments-show() {
+  local ENV_NAME="${1:-}"
+
+  if [[ -n "$ENV_NAME" ]]; then
+    if [[ ! -f "${_CATALYST_ENVS}/${PACKAGE_NAME}/${ENV_NAME}" ]]; then
+      echoerrandexit "No such environment '$ENV_NAME' found for '$PACKAGE_NAME'."
+    fi
+  else
+    if [[ ! -f "${_CATALYST_ENVS}/${PACKAGE_NAME}/curr_env" ]]; then
+      echoerrandexit "No environment selected for '$PACKAGE_NAME'. Try 'catalyst environment select' or 'catalyst environment show <name>'."
+    fi
+    ENV_NAME='curr_env'
+  fi
+  cat "${_CATALYST_ENVS}/${PACKAGE_NAME}/${ENV_NAME}"
+
+  # if [[ -n "$CURR_ENV" ]] && [[ "$CURR_ENV" == "$ENV_NAME" ]]; then
+  #  echo "Current environment:"
+  #  echo "$CURR_ENV"
+  #  echo
+  #fi
+  #local ENV_DB="${_CATALYST_ENVS}/${ENV_NAME}"
+  #if [[ -f "$ENV_DB" ]]; then
+  #  cat "$ENV_DB"
+  #else
+  #  echoerrandexit "No such environment '${ENV_NAME}'."
+  #fi
 }
