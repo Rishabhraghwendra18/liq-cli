a7fcfc53d823061f73dcf7953c1f8f92416baa43
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-28 12:00:44 -0600

    removed migrated commands

--- src/actions/go/actions.sh
+++ src/actions/go/actions.sh
@@ -1,15 +1,3 @@
-go-configure() {
-  if [[ -z "${GOPATH:-}" ]]; then
-    echo "Please provide the 'GOPATH' to the root of go application code, right below 'src'."
-    read -p 'GOPATH: ' GOPATH
-  fi
-  if [[ -z "${REL_GOAPP_PATH:-}" ]]; then
-    echo "Please provide the path to your go application relative to '\$GOPATH/src'."
-    read -p 'Relative app path: ' REL_GOAPP_PATH
-  fi
-  updateCatalystFile
-}
-
 go-get-deps() {
   colorerr "GOPATH=${GOPATH} bash -c 'cd $GOPATH/src/$REL_GOAPP_PATH; go get ./...'"
 }
@@ -20,18 +8,4 @@ go-build() {
 
 go-test() {
   colorerr "GOPATH=${GOPATH} bash -c 'go test $REL_GOAPP_PATH'"
-}
-
-go-start() {
-  colorerr "GOPATH=${GOPATH} bash -c 'cd $GOPATH/src/$REL_GOAPP_PATH; ( dev_appserver.py --enable_watching_go_path=true app.yaml & echo \$! >&3 ) 3> ${BASE_DIR}/go-server.pid 2>&1 | tee ${BASE_DIR}/go-server.log &'"
-}
-
-go-stop() {
-  # TODO: fallback to 'ps aux'
-  colorerr "bash -c '( kill `cat ${BASE_DIR}/go-server.pid` && rm ${BASE_DIR}/go-server.pid ) \
-    || echo \"There may have been a problem shutting down the go dev server. Check manually.\"'"
-}
-
-go-view-log() {
-  less "${BASE_DIR}/go-server.log"
 }
