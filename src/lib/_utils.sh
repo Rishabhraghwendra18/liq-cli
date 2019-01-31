7080c8292c15f84eb89b240d728ccce183f385f4
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-30 19:57:28 -0600

    exclude the 'prelink' saves from service provider searches

--- src/lib/_utils.sh
+++ src/lib/_utils.sh
@@ -486,8 +486,8 @@ pressAnyKeyToContinue() {
 
 getCatPackagePaths() {
   local NPM_ROOT=`npm root`
-  local CAT_PACKAGE_PATHS=`find "$NPM_ROOT"/\@* -maxdepth 2 -name ".catalyst" -exec dirname {} \;`
-  CAT_PACKAGE_PATHS="${CAT_PACKAGE_PATHS} "`find "$NPM_ROOT" -maxdepth 2 -name ".catalyst" -exec dirname {} \;`
+  local CAT_PACKAGE_PATHS=`find "$NPM_ROOT"/\@* -maxdepth 2 -name ".catalyst" -not -path "*.prelink/*" -exec dirname {} \;`
+  CAT_PACKAGE_PATHS="${CAT_PACKAGE_PATHS} "`find "$NPM_ROOT" -maxdepth 2 -name ".catalyst" -not -path "*.prelink/*" -exec dirname {} \;`
 
   echo "$CAT_PACKAGE_PATHS"
 }
