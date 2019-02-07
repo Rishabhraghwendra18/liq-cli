1b103f7d1638deea41c164a8431e82288670e2d8
Zane Rockenbaugh <zane@liquid-labs.com>
2019-02-07 10:26:29 -0600

    long overdue list management func

--- src/lib/_utils.sh
+++ src/lib/_utils.sh
@@ -731,3 +731,17 @@ defineParameters() {
     fi
   done
 }
+
+listAddItem() {
+  local LIST_VAR="${1}"
+  local ITEM="${2}"
+  local SEP="${3:- }"
+
+  if [[ -n "$ITEM" ]]; then
+    if [[ -z "${!LIST_VAR:-}" ]]; then
+      eval $LIST_VAR='"$ITEM"'
+    else
+      eval $LIST_VAR='"$(echo -e "${!LIST_VAR}${SEP}${ITEM}")"'
+    fi
+  fi
+}
