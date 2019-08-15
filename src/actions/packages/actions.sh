55379153ccbd47023225a3b404641f3e672b93eb
Zane Rockenbaugh <zane@liquid-labs.com>
2019-08-14 21:09:19 -0500

    pull out old, fragile link code

--- src/actions/packages/actions.sh
+++ src/actions/packages/actions.sh
@@ -23,76 +23,7 @@ packages-deploy() {
 }
 
 packages-link() {
-  local TMP
-  TMP=$(setSimpleOptions FIX LIST UNLINK -- "$@") \
-    || ( contextHelp; echoerrandexit "Bad options." )
-  eval "$TMP"
-
-  # First, vet the option combinations
-  if [[ -n "${FIX}" ]] && (( ( $_OPTS_COUNT + $# ) > 1 )); then
-    contextHelp
-    echoerrandexit "The '--fix' option is not compatible with other options and takes no arguments."
-  elif [[ -n "${LIST}" ]] && (( ( $_OPTS_COUNT + $# ) > 1 )); then
-    contextHelp
-    echoerrandexit "The '--list' option is not compatible with other options and takes no arguments."
-  elif [[ -n "${UNLINK}" ]] && (( $_OPTS_COUNT > 1 )); then
-    contextHelp
-    echoerrandexit "Cannot combine '--fix' with other options."
-  #Now, execute based on the sub-action indicated by options
-  elif [[ -n "${FIX}" ]]; then
-    local ERROR_COUNT=0
-    local NPM_ROOT=$(npm root)
-    local LINKED_PACKAGE
-    for LINKED_PACKAGE in $(packages-link-list); do
-      echo -n "Checking '$LINKED_PACKAGE'... "
-      if [[ ! -f "${NPM_ROOT}/${LINKED_PACKAGE}.prelink"/package.json ]]; then
-        echoerr "\nThe 'prelink' archive seems to be corrupted! Fix manually."
-        ERROR_COUNT=$(( $ERROR_COUNT + 1 ))
-      else
-        local IS_MOUNTED=$(mount | grep "${NPM_ROOT}/${LINKED_PACKAGE}" || true)
-        if [[ -n "$IS_MOUNTED" ]]; then
-          if [[ -f "${NPM_ROOT}/${LINKED_PACKAGE}"/package.json ]]; then
-            echogreen "Looks good."
-          else
-            echoerr "'$LINKED_PACKAGE' appears mounted, but also corrupt. Fix manually."
-            ERROR_COUNT=$(( $ERROR_COUNT + 1 ))
-          fi
-        else # not mounted
-          echowarn "link has come un-mounted. Attempting fix..."
-          if [[ -e "${NPM_ROOT}/${LINKED_PACKAGE}" ]] && [[ ! -d "${NPM_ROOT}/${LINKED_PACKAGE}" ]]; then
-            echoerr "Expected link directory '${NPM_ROOT}/${LINKED_PACKAGE}' is not a directory as expected. Fix manually."
-            ERROR_COUNT=$(( $ERROR_COUNT + 1 ))
-          elif ! rmdir "${NPM_ROOT}/${LINKED_PACKAGE}"; then
-            echoerr "Could not remove link directory '${NPM_ROOT}/${LINKED_PACKAGE}'. Check if empty and permissions. Automated fix failed."
-            ERROR_COUNT=$(( $ERROR_COUNT + 1 ))
-          elif ! mv "${NPM_ROOT}/${LINKED_PACKAGE}.prelink" "${NPM_ROOT}/${LINKED_PACKAGE}"; then
-            echoerr "mv "${NPM_ROOT}/${LINKED_PACKAGE}.prelink" "${NPM_ROOT}/${LINKED_PACKAGE}" failed while attempting to re-link. Automated fix failed."
-            ERROR_COUNT=$(( $ERROR_COUNT + 1 ))
-          else
-            packages-link $(basename "${LINKED_PACKAGE}")
-          fi
-        fi
-      fi
-    done
-  elif [[ -n "${LIST}" ]]; then
-    packages-link-list
-  elif [[ -n "$UNLINK" ]] && (( $# == 0 )); then
-    packagesUnlinkAll "$BASE_DIR"
-  else # we need to link or unlink specific packages
-    local LINK_SPEC
-    for LINK_SPEC in "$@"; do
-      local UNLINK_PACKAGE_FILE UNLINK_PACKAGE_NAME
-      packages-find-package UNLINK_PACKAGE_FILE UNLINK_PACKAGE_NAME "$LINK_SPEC"
-
-      local LINK_PACKAGE_DIR=$(dirname "$UNLINK_PACKAGE_FILE")
-      local INSTALLED_PACKAGE_DIR="${BASE_DIR}/node_modules/${UNLINK_PACKAGE_NAME}"
-      if [[ -z "$UNLINK" ]]; then
-        packages-link-dolink "$INSTALLED_PACKAGE_DIR" "$LINK_PACKAGE_DIR"
-      else
-        packagesUnlink "${UNLINK_PACKAGE_NAME}"
-      fi
-    done
-  fi
+  echoerrandexit "Linking currently disabled."
 }
 
 packages-lint() {
