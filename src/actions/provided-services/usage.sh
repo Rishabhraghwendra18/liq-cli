887c2ca3f8367c36192e0f2fdd0db104f4b89b42
Zane Rockenbaugh <zane@liquid-labs.com>
2019-08-25 13:37:13 -0500

    mark deprecated interface

--- src/actions/provided-services/usage.sh
+++ src/actions/provided-services/usage.sh
@@ -1,12 +1,14 @@
 usage-provided-services() {
   local PREFIX="${1:-}"
 
-handleSummary "${PREFIX}${cyan_u}provided-services${reset} <action>: Manages package service declarations." || cat <<EOF
+handleSummary "${PREFIX}${red_b}(deprated)${reset} ${cyan_u}provided-services${reset} <action>: Manages package service declarations." || cat <<EOF
 ${PREFIX}${cyan_u}provided-services${reset} <action>:
   ${underline}list${reset} [<package name>...]: Lists the services provided by the named packages or
     all packages in the current repository.
   ${underline}add${reset} [<package name>]: Add a provided service.
   ${underline}delete${reset} [<package name>] <name>: Deletes a provided service.
+
+${red_b}Deprated: These commands will migrate under 'packages'.${reset}
 
 The 'add' action works interactively. Non-interactive alternatives will be
 provided in future versions.
--- src/actions/required-services/usage.sh
+++ src/actions/required-services/usage.sh
@@ -1,12 +1,14 @@
 usage-required-services() {
   local PREFIX="${1:-}"
 
-  handleSummary "${PREFIX}${cyan_u}required-services${reset} <action>: Configures runtime service requirements." || cat <<EOF
+  handleSummary "${PREFIX}${red_b}(deprated)${reset} ${cyan_u}required-services${reset} <action>: Configures runtime service requirements." || cat <<EOF
 ${PREFIX}${cyan_u}required-services${reset} <action>:"
   ${underline}list${reset} [<package name>...]: Lists the services required by the named packages or
     all packages in the current repository.
   ${underline}add${reset} [<package name>]: Add a required service.
   ${underline}delete${reset} [<package name>] <name>: Deletes a required service.
+
+${red_b}Deprated: These commands will migrate under 'packages'.${reset}
 
 The 'add' action works interactively. Non-interactive alternatives will be
 provided in future versions.
