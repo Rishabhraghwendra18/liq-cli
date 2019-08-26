77b5c3089533ba85263fab917c5f3b5205668922
Zane Rockenbaugh <zane@liquid-labs.com>
2019-08-26 12:04:35 -0500

    removed commented code

--- src/actions/meta/lib.sh
+++ src/actions/meta/lib.sh
@@ -12,10 +12,6 @@ metaSetupLiqDb() {
   createDir "$CATALYST_WORK_DB"
   createDir "$_CATALYST_ENV_LOGS"
   createDir "$LIQ_PLAYGROUND"
-  #mkdir -p "$LIQ_DB" || echoerrandexit "Error creating Liquid Development DB at: ${LIQ_DB}"
-  #mkdir -p "$_CATALYST_ENVS" || echoerrandexit "Error creating Liquid Development DB at: ${_CATALYST_ENVS}"
-  #mkdir -p "$CATALYST_WORK_DB" || echoerrandexit "Error creating Liquid Development DB at: ${CATALYST_WORK_DB}"
-  #mkdir -p "$_CATALYST_ENV_LOGS" || echoerrandexit "Error creating Liquid Development DB at: ${_CATALYST_ENV_LOGS}"
   echo -n "Initializing Liquid Dev settings... "
   cat <<EOF > "${LIQ_DB}/settings.sh" || (echo "${red}failed${reset}"; echoerrandexit "Error creating Liquid Development settings.")
 LIQ_PLAYGROUND="$LIQ_PLAYGROUND"
