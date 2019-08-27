61bde46a1a904e02022f11ca471acfa6faf30e0b
Zane Rockenbaugh <zane@liquid-labs.com>
2019-08-26 22:18:28 -0500

    updated help

--- src/actions/meta/help.sh
+++ src/actions/meta/help.sh
@@ -4,7 +4,8 @@ help-meta() {
   handleSummary "${PREFIX}${cyan_u}meta${reset} <action>: Handles catalyst self-config and meta operations." \
    || cat <<EOF
 ${PREFIX}${cyan_u}meta${reset} <action>:
-   ${underline}init${reset} [--silent|-s] [--playground=|-p <absoluter path>]: Creates the Liquid Development DB (a local directory) and playground.
+   ${underline}init${reset} [--silent|-s] [--playground|-p <absolute path>]: Creates the Liquid
+     Development DB (a local directory) and playground.
    ${underline}bash-config${reset}: Prints bash configuration. Try: eval \`catalyst meta bash-config\`
 EOF
 }
--- src/actions/project/help.sh
+++ src/actions/project/help.sh
@@ -3,6 +3,11 @@ help-project() {
 
   handleSummary "${PREFIX}${cyan_u}project${reset} <action>: Project configuration and tools." || cat <<EOF
 ${PREFIX}${cyan_u}project${reset} <action>:
+  ${underline}import${reset} [--url|-u] <package or URL>: Imports the indicated package into your
+    playground. By default, the first arguments are understood as NPM package names and the URL
+    will be retrieved via 'npm view'. If the '--url' option is specified, then the arguments are
+    understood to be git repo URLs, which should contain a 'package.json' file in the repository
+    root.
   ${underline}init${reset}: Configures all packages in the current repository folders as Catalyst
     packages.
   ${underline}publish${reset}: Updates the project site.
