3ca5bb32d2d7844b85bd298370be62c4a9359e33
Zane Rockenbaugh <zane@liquid-labs.com>
2019-11-18 14:59:11 -0500

    update 'project' dir to 'projects'

--- src/integration.test.js
+++ src/integration.test.js
@@ -1,7 +1,7 @@
 import * as testing from './lib/testing'
 import './actions/meta/init.integration-test.js'
-import './actions/project/import.integration-test.js'
-import './actions/project/close.integration-test.js'
+import './actions/projects/import.integration-test.js'
+import './actions/projects/close.integration-test.js'
 // import './actions/work/work.integration-test.js'
 
 beforeAll(testing.setupLocalRepos)
