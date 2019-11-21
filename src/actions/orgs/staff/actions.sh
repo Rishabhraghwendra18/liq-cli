7e5a92697f44a5396899181436a9347c831e294f
Zane Rockenbaugh <zane@liquid-labs.com>
2019-11-21 16:10:52 -0500

    staff through gather phase

--- src/actions/orgs/staff/actions.sh
+++ src/actions/orgs/staff/actions.sh
@@ -1,7 +1,6 @@
 orgs-staff() {
   local ACTION="${1}"; shift
   local CMD="orgs-staff-${ACTION}"
-  echo $CMD
 
   if [[ $(type -t "${CMD}" || echo '') == 'function' ]]; then
     ${CMD} "$@"
@@ -11,5 +10,36 @@ orgs-staff() {
 }
 
 orgs-staff-add() {
-  :
+  local FIELDS="EMAIL FAMILY_NAME GIVEN_NAME START_DATE"
+  local FIELDS_SPEC="${FIELDS}"
+  FIELDS_SPEC="$(echo "$FIELDS_SPEC" | sed -e 's/ /= /g')="
+  eval "$(setSimpleOptions $FIELDS_SPEC NO_CONFIRM:C -- "$@")"
+
+  source "${CURR_ORG_DIR}/public/settings.sh"
+
+  local ALL_SPECIFIED FIELD
+  ALL_SPECIFIED=true
+  for FIELD in $FIELDS; do
+    if [[ -z "${!FIELD}" ]]; then ALL_SPECIFIED=''; break; fi
+  done
+
+  if [[ -z "$ALL_SPECIFIED" ]]; then
+
+    prompter() {
+      local FIELD="$1"
+      local LABEL="$2"
+      if [[ "$FIELD" == 'START_DATE' ]]; then
+        echo "$LABEL (YYYY-MM-DD): "
+      else
+        echo "$LABEL: "
+      fi
+    }
+
+    echo "Adding staff member to ${ORG_COMMON_NAME}:"
+    local OPTS='--prompter=prompter'
+    if [[ -z "$NO_CONFIRM" ]]; then OPTS="${OPTS} --verify"; fi
+    gather-answers ${OPTS} "$FIELDS"
+  fi
+
+  
 }
