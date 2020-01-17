0f8781793d57cfa90076207c3369744673e7ff34
Zane Rockenbaugh <zane@liquid-labs.com>
2020-01-17 12:13:29 -0600

    reorganize and update docs for audits lib [no ci]

--- src/actions/policies/audits/lib/start-lib.sh
+++ src/actions/policies/audits/lib/start-lib.sh
@@ -1,4 +1,24 @@
-# See 'liq help policy audit start' for description of proper input.
+# TODO: link the references once we support.
+# Performs all checks and sets up variables ahead of any state changes. Refer to input confirmation, defaults, and user confirmation functions.
+# outer vars: inherited
+function policy-audit-start-prep() {
+  policy-audit-start-confirm-and-normalize-input-valid "$@"
+  policy-audit-set-defaults
+  policy-audit-start-user-confirm-audit-settings
+}
+
+# TODO: link the references once we support.
+# Initialize an audit. Refer to folder and questions initializers.
+# outer vars: FILE_NAME
+function policy-audit-initialize-records() {
+  local RECORDS_FOLDER
+  policies-audits-initialize-folder
+  policies-audits-initialize-questions
+}
+
+# Internal help functions.
+
+# Lib internal helper. See 'liq help policy audit start' for description of proper input.
 # outer vars: CHANGE_CONTROL FULL DOMAIN
 function policy-audit-start-confirm-and-normalize-input-valid() {
   if [[ -n $CHANGE_CONTROL ]] || [[ -n $FULL ]]; then
@@ -22,7 +42,7 @@ function policy-audit-start-confirm-and-normalize-input-valid() {
   fi
 }
 
-# Sets the outer vars SCOPE, TIME, AUTHOR, and FILE_NAME.
+# Lib internal helper. Sets the outer vars SCOPE, TIME, AUTHOR, and FILE_NAME.
 # outer vars: FULL SCOPE TIME AUTHOR FILE_NAME
 function policy-audit-set-defaults() {
   local FILE_SCOPE FILE_TIME FILE_AUTHOR
@@ -41,7 +61,7 @@ function policy-audit-set-defaults() {
   FILE_NAME="${FILE_TIME}-${DOMAIN}-${SCOPE}-${FILE_AUTHOR}"
 }
 
-# Confirms audit settings unless explicitly told not to.
+# Lib internal helper. Confirms audit settings unless explicitly told not to.
 # outer vars: NO_CONFIRM SCOPE DOMAIN AUTHOR TIME
 function policy-audit-start-user-confirm-audit-settings() {
   echofmt reset "Starting audit with:\n\n* scope: ${bold}${SCOPE}${reset}\n* domain: ${bold}${DOMAIN}${reset}\n* author: ${bold}${AUTHOR}${reset}\n"
@@ -54,16 +74,7 @@ function policy-audit-start-user-confirm-audit-settings() {
   fi
 }
 
-# TODO: link the references once we support.
-# Refer to input confirmation, defaults, and user confirmation functions.
-# outer vars: inherited
-function policy-audit-start-prep() {
-  policy-audit-start-confirm-and-normalize-input-valid "$@"
-  policy-audit-set-defaults
-  policy-audit-start-user-confirm-audit-settings
-}
-
-# Determines and creates the RECORDS_FOLDER
+# Lib internal helper. Determines and creates the RECORDS_FOLDER
 # outer vars: RECORDS_FOLDER
 function policies-audits-initialize-folder() {
   RECORDS_FOLDER="$(orgsPolicyRepo)/records/${FILE_NAME}"
@@ -74,7 +85,7 @@ function policies-audits-initialize-folder() {
   mkdir -p "$RECORDS_FOLDER"
 }
 
-# Determines applicable questions and generates initial TSV record.
+# Lib internal helper. Determines applicable questions and generates initial TSV record.
 function policies-audits-initialize-questions() {
   FILES="$(policiesGetPolicyFiles --find-options "-path './policies/$DOMAIN/standards/*items.tsv'")"
 
@@ -85,13 +96,4 @@ function policies-audits-initialize-questions() {
   done <<< "$FILES"
 
   echoerrandexit "Implement..."
-}
-
-# TODO: link the references once we support.
-# Initialize an audit. Refer to folder and questions initializers.
-# outer vars: FILE_NAME
-function policy-audit-initialize-records() {
-  local RECORDS_FOLDER
-  policies-audits-initialize-folder
-  policies-audits-initialize-questions
 }
