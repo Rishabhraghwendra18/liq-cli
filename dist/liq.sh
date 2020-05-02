4ecd897923ed80214938b4f57241341aed00969c
Zane Rockenbaugh <zane@liquid-labs.com>
2020-05-01 21:12:21 -0500

    updated orgs audits help [no ci]

--- dist/liq.sh
+++ dist/liq.sh
@@ -3158,25 +3158,25 @@ orgs-audits-start() {
     orgs-audits-finalize-session "${AUDIT_PATH}" "${TIME}" "$(orgs-audits-describe)"
   fi
 }
-# TODO: instead, create simple spec; generate 'completion' options and 'docs' from spec.
-
 help-orgs-audits() {
-  handleSummary "${cyan_u}orgs audits${reset} <action>: Manage audits." || cat <<EOF
-${cyan_u}policies audits${reset} <action>:
-$(help-orgs-audits-start | sed -e 's/^/  /')
+  local PREFIX="${1:-}"
+
+  local SUMMARY="Audit management."
+
+  handleSummary "${cyan_u}orgs audits${reset} <action>: ${SUMMARY}" || cat <<EOF
+${cyan_u}orgs audits${reset} <action>:
+  ${SUMMARY}
+$(_help-actions-list orgs-audits start | indent)
 EOF
-} #$'' HACK to reset Atom Beutifier
+}
 
 help-orgs-audits-start() {
-  cat <<EOF
-${underline}start${reset} [--scope|-s <scope>] [--no-confirm|-C] [<domain>] :
-  Initiates an audit. An audit scope is either 'change' (default), 'process' or 'full'.
+  cat <<EOF | _help-func-summary start "[--scope|-s <scope>] [--no-confirm|-C] [<domain>]"
+Initiates an audit. An audit scope is either 'change' (default), 'process' or 'full'.
 
-  Currently supported domains are 'code' and 'network'. If domain isn't specified, then the user will be given an
-  interactive list.
+Currently supported domains are 'code' and 'network'. If domain isn't specified, then the user will be given an interactive list.
 
-  By default, a summary of the audit will be displayed to the user for confirmation. This can be supressed with
-  the '--no-confirm' option.
+By default, a summary of the audit will be displayed to the user for confirmation. This can be supressed with the '--no-confirm' option.
 EOF
 }
 # Generates a human readable description string based on audit parameters. The '--short' option guarantees a name compatible with branch naming conventions and suitable for use with 'liq work start'.
