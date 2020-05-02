e1d0e9c3ba2c474d6c11b4b2b091b2ca0cfee895
Zane Rockenbaugh <zane@liquid-labs.com>
2020-05-01 21:34:58 -0500

    update 'orgs policies' help [no ci]

--- dist/liq.sh
+++ dist/liq.sh
@@ -3508,15 +3508,23 @@ orgs-policies-update() {
 help-orgs-policies() {
   local PREFIX="${1:-}"
 
-  handleSummary "${PREFIX}${cyan_u}orgs policies${reset} <action>: Manages organization policies." || cat <<EOF
-Policies defines all manner of organizational operations. They are "organizational code".
+  local SUMMARY="Manages organization policies."
 
-${PREFIX}${cyan_u}policies${reset} <action>:
-  ${underline}document${reset}: Refreshes (or generates) org policy documentation based on current data.
-  ${underline}update${reset}: Updates organization policies.
+  handleSummary "${PREFIX}${cyan_u}orgs policies${reset} <action>: ${SUMMARY}" || cat <<EOF
+$(echo "${SUMMARY} Policies defines all manner of organizational operations. They are \"organizational code\"." | fold -sw 80 | indent)
+$(_help-actions-list orgs-policies document update | indent)
+EOF
+}
 
-${bold}Sub-resources${reset}:
-  * $( SUMMARY_ONLY=true; help-orgs-audits )
+help-orgs-policies-document() {
+  cat <<EOF | _help-func-summary document
+Refreshes (or generates) org policy documentation based on current data.
+EOF
+}
+
+help-orgs-policies-update() {
+  cat <<EOF | _help-func-summary update
+Updates organization policies.
 EOF
 }
 # Retrieves the policy directories from the current org. Currenly requires sensitive until we think through the
