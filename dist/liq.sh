df70bb9ceafb45427c61088ae18b577541140af8
Zane Rockenbaugh <zane@liquid-labs.com>
2020-05-04 16:38:48 -0500

    hotfix: update meta-keys help

--- dist/liq.sh
+++ dist/liq.sh
@@ -2824,18 +2824,18 @@ Expire-Date: ${EXPIRY_YEARS}y
 # TODO: instead, create simple spec; generate 'completion' options and 'docs' from spec.
 
 help-meta-keys() {
-  handleSummary "${cyan_u}keys${reset} <action>: Manage user keys." || cat <<EOF
+  local SUMMARY="Manage user keys."
+
+  handleSummary "${cyan_u}keys${reset} <action>: ${SUMMARY}" || cat <<EOF
 ${cyan_u}meta keys${reset} <action>:
-$(help-meta-keys-create | sed -e 's/^/  /')
+  ${SUMMARY}
+$(_help-actions-list meta-keys create | indent)
 EOF
-} #$'' HACK to reset Atom Beutifier
+}
 
 help-meta-keys-create() {
-  cat <<EOF
-${underline}create${reset} [--import|-i] [--user|-u <email>] [--full-name|-f <full name>] :
-  Creates a PGP key appropriate for use with liq. The user (email) and their full name will be extracted from the
-  git config 'user.email' and 'user.name' if not specified. In general, you should configure the git parameters
-  because that's what will be used by other liq funcitons.
+  cat <<EOF | _help-func-summary create "[--import|-i] [--user|-u <email>] [--full-name|-f <full name>]"
+Creates a PGP key appropriate for use with liq. The user (email) and their full name will be extracted from the git config 'user.email' and 'user.name' if not specified. In general, you should configure the git parameters because that's what will be used by other liq funcitons.
 EOF
 }
 meta-keys-user-has-key() {
