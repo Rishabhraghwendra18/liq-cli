3c3f1259935bca70bbc6ab0a7d84b1dd0f477da6
Zane Rockenbaugh <zane@liquid-labs.com>
2020-05-05 10:34:36 -0500

    reworked structure [no ci]

--- src/completion.sh
+++ src/completion.sh
@@ -2,96 +2,120 @@
 # changes to take effect.
 
 # TODO: we could generate this from the help docs... make the spec central!
-_liq()
-{
-    local CUR # the current token; this may be blank and used to expand partials; provided my complete routine
-    local PREV # the last completed token; provided by complete routine
-    local OPTS # the options, set by this func, for completion
-    local GLOBAL_ACTIONS="help"
-    # Using 'GROUPS' was causing errors; set by some magic.
-    local ACTION_GROUPS="data environments meta orgs policies projects remotes required-services services work"
-    COMPREPLY=()
-    local WORD_COUNT=${#COMP_WORDS[@]}
-    # TODO: instead of simple 'CUR/PREV', use the above to see where in the
-    # command we are. This will allow us to implement 'exhaustive' completion.
-    # Switch on what we need: group, action, action OPTS, or action args.
-    CUR="${COMP_WORDS[COMP_CWORD]}"
-    PREV="${COMP_WORDS[COMP_CWORD-1]}"
-    GROUP="${COMP_WORDS[1]}"
-    ACTION="${COMP_WORDS[2]}"
+_liq() {
+  local GLOBAL_ACTIONS="help"
+  # Using 'GROUPS' was causing errors; set by some magic.
+  local ACTION_GROUPS="data environments meta orgs projects services work"
 
-    if (( $WORD_COUNT <= 3 )); then
-      case "${PREV}" in
-        liq)
-          OPTS="${GLOBAL_ACTIONS} ${ACTION_GROUPS}";;
-        # globals
-        help)
-          OPTS="${ACTION_GROUPS}";;
-        # groups
-        data)
-          OPTS="build clear load rebuild reset";;
-        environments)
-          OPTS="add delete deselect list select set show update";;
-        meta)
-          OPTS="init bash-config keys";;
-        orgs)
-          OPTS="affiliate create list show select staff";;
-				policies)
-					OPTS="document audits";;
-        projects)
-          OPTS="build close create issues publish qa sync test services";;
-        remotes)
-          OPTS="add delete set-main";;
-        required-services) # deprecated
-          OPTS="list add delete";;
-        services)
-          OPTS="connect err-log list log restart start stop";;
-        work)
-          OPTS="diff-master edit ignore-rest involve issues links list merge qa report resume save stage start status stop submit sync";;
-      esac
-    else
-      case "${GROUP}" in
-        meta)
-          if [[ "${ACTION}" == 'keys' ]] && (( $WORD_COUNT == 4 )); then
-            OPTS="create"
-          fi;;
-        orgs)
-          if [[ "${ACTION}" == staff ]] && (( $WORD_COUNT == 4 )); then
-            OPTS="add list remove org-chart"
-          else
-            case "${ACTION}" in
-              select)
-                COMPREPLY=( $(compgen -W "$(find ~/.liquid-development/orgs -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)" -- ${CUR}) )
-                return 0;;
-            esac
-          fi;;
-        policies)
-          if [[ "${ACTION}" == 'audits' ]] && (( $WORD_COUNT == 4 )); then
-            OPTS="start"
-          elif [[ ${COMP_WORDS[3]} == 'start' ]] && (( $WORD_COUNT == 5 )); then
-            OPTS="code network"
-          fi;;
-        projects)
-          case "${ACTION}" in
-            services)
-              OPTS="add list delete show";;
-            issues)
-              OPTS="show";;
-          esac ;; # projects-actions
-        work)
-          case "${ACTION}" in
-            links)
-              OPTS="add list remove";;
-            stage)
-              COMPREPLY=( $(compgen -o nospace -W "$(for d in ${CUR}*; do [[ -d "$d" ]] && echo $d/ || echo $d; done)" -- ${CUR}) )
-              return 0
-            ;;
-          esac ;;# work-actions
-      esac
-    fi
+  local TOKEN COMP_FUNC CUR
+  CUR="${COMP_WORDS[COMP_CWORD]}"
+  COMP_FUNC='comp'
+  local WORD_COUNT=${#COMP_WORDS[@]}
+  local TOKEN_COUNT=0
 
+  no-opts() {
+    COMPREPLY=( $(compgen -W "" -- ${CUR}) )
+  }
+
+  std-reply() {
     COMPREPLY=( $(compgen -W "${OPTS}" -- ${CUR}) )
-    return 0
+  }
+
+  comp-liq() {
+    OPTS="${GLOBAL_ACTIONS} ${ACTION_GROUPS}"; std-reply
+  }
+
+  comp-liq-help() {
+    OPTS="${ACTION_GROUPS}"; std-reply
+  }
+
+  comp-func-builder() {
+    local TOKEN_PATH="${1}"
+    local VAR_KEY="${2}"
+    local OPT
+    local ACTIONS_VAR="${VAR_KEY}_ACTIONS"
+    local GROUPS_VAR="${VAR_KEY}_GROUPS"
+    echo "comp-liq-${TOKEN_PATH}() { OPTS=\"${!ACTIONS_VAR:-} ${!GROUPS_VAR:-}\"; std-reply; }"
+    echo "comp-liq-help-${TOKEN_PATH}() { comp-liq-${TOKEN_PATH}; }"
+    for OPT in ${!ACTIONS_VAR}; do
+      echo "function comp-liq-${TOKEN_PATH}-${OPT}() { no-opts; }"
+      echo "function comp-liq-help-${TOKEN_PATH}-${OPT}() { no-opts; }"
+    done
+  }
+
+  # data group
+  local DATA_ACTIONS="build clear load rebuild reset"
+  eval "$(comp-func-builder 'data' 'DATA')"
+
+  # environments group
+  local ENVIRONMENTS_ACTIONS="add delete deselect list select set show update"
+  eval "$(comp-func-builder 'environments' 'ENVIRONMENTS')"
+
+  # meta group
+  local META_ACTIONS="bash-config init"
+  local META_GROUPS="keys"
+  eval "$(comp-func-builder 'meta' 'META')"
+
+  META_KEYS_ACTIONS="create"
+  eval "$(comp-func-builder 'meta-keys' 'META_KEYS')"
+
+  local ORGS_ACTIONS="affiliate create list show select"
+  local ORGS_GROUPS="audits policies staff"
+  eval "$(comp-func-builder 'orgs' 'ORGS')"
+  comp-liq-orgs-select() {
+    COMPREPLY=( $(compgen -W "$(find ~/.liquid-development/orgs -maxdepth 1 -mindepth 1 -type l -exec basename {} \;)" -- ${CUR}) )
+  }
+
+  local ORGS_STAFF_ACTIONS="add list remove org-chart"
+  eval "$(comp-func-builder 'orgs-staff' 'ORGS_STAFF')"
+
+  local ORGS_POLICIES_ACTIONS="document update"
+  eval "$(comp-func-builder 'orgs-policies' 'ORGS_POLICIES')"
+
+  local ORGS_AUDITS_ACTIONS="start"
+  eval "$(comp-func-builder 'orgs-audits' 'ORGS_AUDITS')"
+  # TODO: support 'code' and 'network' type completion for start
+
+  local PROJECTS_ACTIONS="build close create publish qa sync test"
+  local PROJECTS_GROUPS="issues sevices"
+  eval "$(comp-func-builder 'projects' 'PROJECTS')"
+
+  local PROJECTS_ISSUES_ACTIONS="show"
+  eval "$(comp-func-builder 'projects-issues' 'PROJECTS_ISSUES')"
+
+  local PROJECTS_SERVICES_ACTIONS="add list delete show"
+  eval "$(comp-func-builder 'projects-services' 'PROJECTS_SERVICES')"
+
+  local SERVICES_ACTIONS="connect err-log list log restart start stop"
+  eval "$(comp-func-builder 'services' 'SERVICES')"
+
+  local WORK_ACTIONS="diff-master edit ignore-rest involve list merge qa report resume save stage start status stop submit sync"
+  local WORK_GROUPS="issues links"
+  eval "$(comp-func-builder 'work' 'WORK')"
+  comp-liq-work-stage() {
+    COMPREPLY=( $(compgen -o nospace -W "$(for d in ${CUR}*; do [[ -d "$d" ]] && echo $d/ || echo $d; done)" -- ${CUR}) )
+  }
+
+  local WORK_ISSUES_ACTIONS="add list remove"
+  eval "$(comp-func-builder 'work-issues' 'WORK_ISSUES')"
+
+  local WORK_LINKS_ACTIONS="add list remove"
+  eval "$(comp-func-builder 'work-links' 'WORK_LINKS')"
+
+  for TOKEN in ${COMP_WORDS[@]}; do
+    if [[ "$TOKEN" != -* ]] && (( $TOKEN_COUNT + 1 < $WORD_COUNT )); then
+      local TYPE="$(type -t "${COMP_FUNC}-${TOKEN}" || echo '')"
+      if [[ "${TYPE}" == 'function' ]]; then
+        COMP_FUNC="${COMP_FUNC}-${TOKEN}"
+        TOKEN_COUNT=$(( $TOKEN_COUNT + 1 ))
+      fi
+    else
+      TOKEN_COUNT=$(( $TOKEN_COUNT + 1 ))
+    fi
+  done
+
+  $COMP_FUNC
+  return 0
 }
 
 complete -F _liq liq
