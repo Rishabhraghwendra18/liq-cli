f125ad897129050bbfa2f7ffddd8b5cf5fa4df91
Zane Rockenbaugh <zane@liquid-labs.com>
2020-06-02 22:37:18 -0500

    label sync implemented [no ci]

--- dist/liq.sh
+++ dist/liq.sh
@@ -136,6 +136,24 @@ list-get-item() {
   done <<< "${!LIST_VAR}"
 }
 
+# Echoes the frist item in the named list matching the given prefix.
+#
+# Example:
+# LIST="foo bar"$'\n'"foo baz"
+# list-get-item-by-prefix LIST "foo " # echoes 'foo bar'
+list-get-item-by-prefix() {
+  local LIST_VAR="${1}"
+  local PREFIX="${2}"
+
+  local ITEM
+  while read -r ITEM; do
+    if [[ "${ITEM}" == "${PREFIX}"* ]] ; then
+      echo -n "${ITEM%\\n}"
+      return
+    fi
+  done <<< "${!LIST_VAR}"
+}
+
 # Joins a list with a given string and echos the result. We use 'echo -e' for the join string, so '\n', '\t', etc. will
 # work.
 #
@@ -196,7 +214,7 @@ list-rm-item() {
     ITEM=${ITEM//./\\.}
     ITEM=${ITEM//[/\\[}
     # echo "ITEM: $ITEM" >&2
-    NEW_ITEMS="$(echo "${!LIST_VAR}" | sed -e '\#^'$ITEM'$#d')"
+    NEW_ITEMS="$(echo "${!LIST_VAR}" | sed -e '\#^'"${ITEM}"'$#d')"
     eval $LIST_VAR='"'"$NEW_ITEMS"'"'
   done
 }
@@ -2218,7 +2236,7 @@ projects-qa() {
 }
 
 projects-setup() {
-  eval "$(setSimpleOptions PROJECT= -- "$@")" \
+  eval "$(setSimpleOptions DELETE PROJECT= NO_UPDATE:U FORCE_UPDATE: -- "$@")" \
     || { contextHelp; echoerrandexit "Bad options."; }
 
   if [[ -z "$PROJECT" ]]; then
@@ -2254,13 +2272,13 @@ projects-setup() {
   source "${LIQ_PLAYGROUND}/${ORG_PROJECT}/settings.sh"
   if [[ -z "${PROJECT_LABELS:-}" ]]; then
     echo "No project labels defined; using default label set..."
-    PROJECT_LABELS=$(cat << EOF
-bounty
-bug
-enhancement
-good first issue
-needs spec
-optimization
+    PROJECT_LABELS=$(cat <<EOF
+bounty:This task offers a bounty:209020
+bug:Something is broken:d73a4a
+enhancement:New feature or request:a2eeef
+good first issue:Good for newcomers:7050ff
+needs spec:The task as-is is not fully specified:ff4040
+optimization:Make better without changing behavior:00dd70
 EOF
     )
   fi
@@ -2274,13 +2292,77 @@ EOF
     echoerrandexit "'repository.url' from 'package.json' in unknown format; only github currently supported."
   fi
 
-  local CURR_LABELS
-  CURR_LABELS="$(hub api "/repos/${GIT_BASE}/labels" | jq -r '.[].name')"
+  local CURR_LABEL_DATA CURR_LABELS TEST_LABEL INDEX PROJECT_LABEL_NAMES
+  PROJECT_LABEL_NAMES="$(echo "${PROJECT_LABELS}" | awk -F: '{print $1}')"
+  CURR_LABEL_DATA="$(hub api "/repos/${GIT_BASE}/labels")"
+  CURR_LABELS="$(echo "$CURR_LABEL_DATA" | jq -r '.[].name' )"
 
-  # TODO
-  echo "$PROJECT_LABELS"
-  echo '------------------'
-  echo "$CURR_LABELS"
+  local NON_STD_LABELS="${CURR_LABELS}"
+  while read -r TEST_LABEL; do
+    INDEX=$(list-get-index NON_STD_LABELS "${TEST_LABEL}")
+    if [[ -n "${INDEX}" ]]; then
+      list-rm-item NON_STD_LABELS "${TEST_LABEL}"
+    fi
+  done <<< "${PROJECT_LABEL_NAMES}"
+
+  MISSING_LABELS="${PROJECT_LABEL_NAMES}"
+  while read -r TEST_LABEL; do
+    INDEX=$(list-get-index MISSING_LABELS "${TEST_LABEL}")
+    if [[ -n "${INDEX}" ]]; then
+      list-rm-item MISSING_LABELS "${TEST_LABEL}"
+    fi
+  done <<< "${CURR_LABELS}"
+
+  local LABELS_SYNCED=true
+  if [[ -n "${NON_STD_LABELS}" ]]; then
+    if [[ -n "${DELETE}" ]]; then
+      while read -r TEST_LABEL; do
+        echo "Removing non-standard label '${TEST_LABEL}'..."
+        hub api -X DELETE "/repos/${GIT_BASE}/labels/${TEST_LABEL}"
+      done <<< "${NON_STD_LABELS}"
+    else
+      echowarn "The following non-standard labels where found in ${PROJECT}:\n$(echo "${NON_STD_LABELS}" | awk '{ print "* "$0 }')\n\nInclude the '--delete' option to attempt removal."
+      LABELS_SYNCED=false
+    fi
+  fi # non-standard label check; potential deletion
+
+  local LABEL_SPEC NAME COLOR DESC
+  set-spec() {
+    NAME="$(echo "${LABEL_SPEC}" | awk -F: '{print $1}')"
+    DESC="$(echo "${LABEL_SPEC}" | awk -F: '{print $2}')"
+    COLOR="$(echo "${LABEL_SPEC}" | awk -F: '{print $3}')"
+  }
+
+  if [[ -n "${MISSING_LABELS}" ]]; then
+    while read -r TEST_LABEL; do
+      LABEL_SPEC="$(list-get-item-by-prefix PROJECT_LABELS "${TEST_LABEL}:")"
+      set-spec
+      echo "Adding label '${TEST_LABEL}'..."
+      hub api -X POST "/repos/${GIT_BASE}/labels" -f name="${NAME}" -f description="${DESC}" -f color="${COLOR}"
+    done <<< "$MISSING_LABELS"
+  fi # missing labels creation
+
+  if [[ -z "$NO_UPDATE" ]] && [[ "${LABELS_SYNCED}" == true ]] || [[ -n "$FORCE_UPDATE" ]]; then
+    [[ "${LABELS_SYNCED}" != true ]] || echo "Label names synchronized."
+    echo "Checking label definitions..."
+    LABELS_SYNCED=false
+    while read -r LABEL_SPEC; do
+      set-spec
+      local CURR_DESC CURR_COLOR
+      CURR_DESC="$(echo "$CURR_LABEL_DATA" | jq -r "map(select(.name == \"${NAME}\"))[0].description")"
+      CURR_COLOR="$(echo "$CURR_LABEL_DATA" | jq -r "map(select(.name == \"${NAME}\"))[0].color")"
+      if [[ "${CURR_COLOR}" != "${COLOR}" ]] || [[ "$CURR_DESC" != "${DESC}" ]]; then
+        echo "Updating label definition for '${NAME}'..."
+        hub api -X PATCH "/repos/${GIT_BASE}/labels/${NAME}" -f description="${DESC}" -f color="${COLOR}" > /dev/null
+        LABELS_SYNCED=true
+      fi
+    done <<< "${PROJECT_LABELS}"
+
+    [[ "$LABELS_SYNCED" == true ]] && echo "Label definitions updated." || echo "Labels already up-to-date."
+  else
+    [[ "${LABELS_SYNCED}" != true ]] || [[ -n "$NO_UPDATE" ]] || echo "Labels not synchronized; skipping update."
+    [[ -z "${NO_UPDATE}" ]] || echo "Skipping update."
+  fi # labels definition check and update
 }
 
 # see: liq help projects sync
