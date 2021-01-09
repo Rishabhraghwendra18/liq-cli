633e3710652f95b29f0c0bdca830faaf539b082b
Zane Rockenbaugh <zane@liquid-labs.com>
2021-01-08 20:28:04 -0600

    hotfix: update settings to new location

--- src/actions/orgs/lib.sh
+++ src/actions/orgs/lib.sh
@@ -1,19 +1,5 @@
-# sources the current org settings, if any
-sourceCurrentOrg() {
-  local REL_DIR
-  if [[ -d org_settings ]]; then
-    REL_DIR="."
-  elif [[ -n "$BASE_DIR" ]]; then
-    REL_DIR="$BASE_DIR/.."
-  else
-    echoerrandexit "Cannot get current organization outside of project context."
-  fi
-
-  source "${REL_DIR}/org_settings/settings.sh"
-  if [[ -d "${REL_DIR}/org_settings_sensitive" ]]; then
-    source "${REL_DIR}/org_settings_sensitive/settings.sh"
-  fi
-}
+# deprecated; use orgs-lib-source-settings
+# sourceCurrentOrg() {
 
 # Retrieves the policy dir for the named NPM org or will infer from context. Org base and, when private, policy projects
 # must be locally available.
@@ -36,10 +22,11 @@ orgs-lib-source-settings() {
   fi
 
   if [[ -e "$LIQ_ORG_DB/${NPM_ORG}" ]]; then
-    [[ -f "$LIQ_ORG_DB/${NPM_ORG}/settings.sh" ]] || echoerrandexit "Could not locate settings file for '${NPM_ORG}'."
-    source "$LIQ_ORG_DB/${NPM_ORG}/settings.sh"
+    local SETTINGS="${LIQ_ORG_DB}/${NPM_ORG}/data/orgs/settings.sh"
+    [[ -f "${SETTINGS}" ]] || echoerrandexit "Could not locate settings file for '${NPM_ORG}' (${SETTINGS})."
+    source "${SETTINGS}"
 
-    ORG_CHART_TEMPLATE="${ORG_CHART_TEMPLATE/\~/$LIQ_PLAYGROUND}"
+    ORG_CHART_TEMPLATE="${ORG_CHART_TEMPLATE/\~/$LIQ_PLAYGROUND}" # TODO: huh? explain this...
   else
     echoerrandexit "Did not find expected base org package. Try:\nliq orgs import <pkg || URL>"
   fi
