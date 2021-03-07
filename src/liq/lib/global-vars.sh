1d808a509e97e4428d7453f809f3312856a4a04d
Zane Rockenbaugh <zane@liquid-labs.com>
2021-03-07 16:59:21 -0600

    broke out shared vars

--- src/liq/lib/global-vars.sh
+++ src/liq/lib/global-vars.sh
@@ -1,14 +1,7 @@
+###
 # Global constants.
-LIQ_NPM_KEY="liq"
-LIQ_DB_BASENAME=".${LIQ_NPM_KEY}"
-LIQ_DB="${HOME}/${LIQ_DB_BASENAME}"
-LIQ_SETTINGS="${LIQ_DB}/settings.sh"
-LIQ_ENV_DB="${LIQ_DB}/environments"
-LIQ_ORG_DB="${LIQ_DB}/orgs"
-LIQ_WORK_DB="${LIQ_DB}/work"
-LIQ_EXTS_DB="${LIQ_DB}/exts"
-LIQ_ENV_LOGS="${LIQ_DB}/logs"
-LIQ_PLAYGROUND="${LIQ_DB}/playground"
+###
+source ../../shared/common-globals.sh
 
 LIQ_DIST_DIR="$(dirname "$(real_path "${0}")")"
 
