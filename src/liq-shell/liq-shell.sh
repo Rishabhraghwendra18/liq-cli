9c09c51e7c57003285292b3143bd290c46b20762
Zane Rockenbaugh <zane@liquid-labs.com>
2021-03-08 23:29:35 -0600

    support necessary in-shell function call

--- src/liq-shell/liq-shell.sh
+++ src/liq-shell/liq-shell.sh
@@ -18,10 +18,23 @@ fi
 
 PS4='$(echo $(date +"%Y/%m/%d (%H:%M)") $(history 1) >> /tmp/trace.txt)'
 PS1='liq> '
+export HISTFILE=/home/liq/.liq/shell-history
 set show-all-if-ambiguous on
 # the _liq function should have been sourced as part of the users
 complete -D -F _liq
 complete -I -F _liq
+
+# In the shell, some functions are run directly in the shell, so we need to source everything.
+SOURCE="$(npm explore -g @liquid-labs/liq-cli -- pwd)/dist/liq-source.sh"
+export LIQ_NO_TRAP=true
+source "${SOURCE}"
+liq-init-exts "$@"
+
+# Un-set the errexit and nounset; these are incompatblie with bash-preexec.
+set +o errexit
+set +o nounset
+# Notice we retain 'pipefail'. We want error propogation to happen the same, we just react different once it bubbles
+# out.
 
 # bind "TAB:menu-complete"
 bind "set show-all-if-ambiguous on"
@@ -34,9 +47,14 @@ preexec() {
 
   if [[ "${1}" == '\'* ]]; then
     eval ${1:1}
+  elif [[ "${1}" == 'projects focus'* ]]; then
+    COMMANDS=${1/projects focus/}
+    # Need to kill error tracing and override 'echoerrandexit' exit behavior because these commands are run directly in
+    # the shell.
+    ECHO_NEVER_EXIT=true projects-focus ${COMMANDS}
   else
     # echo -e "executing:\nliq ${1}"
-    liq $1
+    liq ${1}
   fi
   return 1
 }
