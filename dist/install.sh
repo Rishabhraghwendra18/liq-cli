53d066fe126cb50214eb575e6e78e0c4f725dfd2
Zane Rockenbaugh <zane@liquid-labs.com>
2021-03-08 23:37:21 -0600

    rebuild dist [no ci]

--- dist/install.sh
+++ dist/install.sh
@@ -430,14 +430,20 @@ echowarn() {
 # Echoes a formatted message to STDERR. The default exit code is '1', but if 'EXIT_CODE', then that will be used. E.g.:
 #
 #    EXIT_CODE=5
-#    echoerrandexit "Fatal code 5!"
+#    echoerrandexit "Fatal code 5
+#
+# 'ECHO_NEVER_EXIT' can be set to any non-falsy value to supress the exit. This is intended primarily for use when liq
+# functions are sourced and called directly from within the main shell, in which case exiting would kill the entire
+# terminal session.
 #
 # See echofmt for further options and details.
 echoerrandexit() {
   echofmt --error "$@"
 
-  [[ -z "${EXIT_CODE:-}" ]] || exit ${EXIT_CODE}
-  exit 1
+  if [[ -z "${ECHO_NEVER_EXIT:-}" ]]; then
+    [[ -z "${EXIT_CODE:-}" ]] || exit ${EXIT_CODE}
+    exit 1
+  fi
 }
 
 echo "Starting liq install..."
@@ -525,8 +531,10 @@ colorerrbg() {
 
 # Verifies access to github.
 check-git-access() {
+  eval "$(setSimpleOptions NO_EXIT -- "$@")"
   # if we don't supress the output, then we get noise even when successful
   ssh -qT git@github.com 2> /dev/null || if [ $? -ne 1 ]; then
+    [[ -z "NO_EXIT" ]] || return 1
     echoerrandexit "Could not connect to github; try to add add your GitHub key like:\nssh-add /example/path/to/key"
   fi
 }
