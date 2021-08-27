3e8e425b0f266402ff168e5db805ad9e00300a9b
Zane Rockenbaugh <zane@liquid-labs.com>
2021-08-27 11:00:18 -0500

    bugfix: fix call to get epoch seconds

--- src/liq/actions/work/changelog/lib-changelog-action-add-entry.js
+++ src/liq/actions/work/changelog/lib-changelog-action-add-entry.js
@@ -1,11 +1,9 @@
-import dateFormat from 'dateformat'
-
 import { readChangelog, requireEnv, saveChangelog } from './lib-changelog-core'
 
 const createNewEntry = (changelog) => {
   // get the approx start time according to the local clock
   const now = new Date()
-  const startTimestamp = dateFormat(now, 'UTC:yyyy-mm-dd-HHMM.ss Z')
+  const startTimestamp = now.toISOString()
   const startEpochMillis = now.getTime()
   // process the 'work unit' data
   const issues = requireEnv('WORK_ISSUES').split('\n')
